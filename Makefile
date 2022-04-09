e42745251113017a0daaa627ad82099a4dff1b81
Ask Bjørn Hansen <ask@develooper.com>
2022-04-08 21:15:18 -0700

    Update publish to build dependencies
    #ntppool #locationcode

--- Makefile
+++ Makefile
@@ -1,15 +1,6 @@
-.PHONY: test
-
-PUSH=helm cm-push --debug
-
 .PHONY: publish
 publish:
-	@echo run helm push
-	@helm plugin install https://github.com/chartmuseum/helm-push.git
-	helm --username "$$HELM_USERNAME" --password="$$HELM_PASSWORD" repo add ntppool ${HELM_URL}
-	helm repo update
-	${PUSH} charts/splash  ntppool
-	${PUSH} charts/ntppool ntppool
-	${PUSH} charts/geoip   ntppool
-	${PUSH} charts/smtp    ntppool
-	${PUSH} charts/locationcode ntppool
+	./publish.pl
+
+
+.PHONY:
---
+++ publish.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/env perl
+use v5.30.0;
+use warnings;
+use Carp qw(croak);
+
+my %repos = (
+    "k8s-at-home-library" => "https://library-charts.k8s-at-home.com",
+    "stable"              => "https://charts.helm.sh/stable",
+);
+
+
+chdir "charts" or die "could not chdir to charts: $!";
+
+my @charts = grep { -d $_ } glob "*";
+
+my @publish;
+
+my $helm_url      = $ENV{HELM_URL};
+
+my $commit = $ENV{DRONE_COMMIT_MESSAGE};
+
+$commit =~ m/^(\S+):/sm;
+push @publish, $1 if $1;
+
+push @publish, ($commit =~ m/\#(\S+)/gsm);
+
+my %charts = map { $_ => 1 } @charts;
+
+@publish = grep { my $ok = $charts{$_}; $charts{$_} = 0; $ok } @publish;
+
+say "publish ", join ", ", @publish;
+
+run(qq[helm --username "\$HELM_USERNAME" --password="\$HELM_PASSWORD" repo add ntppool ${helm_url}],
+    {silent => 1}
+    );
+
+for my $repo (sort keys %repos) {
+    run(qq[helm repo add "$repo" "$repos{$repo}"]);
+}
+
+run("helm repo update");
+
+for my $chart (@publish) {
+    run("helm dependency build $chart");
+    run("helm cm-push $chart ntppool");
+}
+
+
+sub run {
+    my @ar    = @_;
+    my $parms = ref $ar[-1] eq "HASH" ? pop @ar : {};
+
+    print "Running: ", join(" ", @ar), "\n" unless $parms->{silent};
+
+    return 1 if system(@ar) == 0;
+
+    my $exit_value = $? >> 8;
+    return 0
+      if $parms->{fail_silent_if}
+      && $exit_value == $parms->{fail_silent_if};
+
+    my $msg = "system @ar failed: $exit_value ($?)";
+    croak($msg) unless $parms->{failok};
+    print "$msg\n";
+    return 0;
+}
